cccbda0ad32e7db2d59ae0d519f942fcbd22da5a
Tatev84 <tatevh84@gmail.com>
2021-07-11 22:32:05 +0400

    js homework

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+/* var arr=["Karen", "Hayk", "Ani"];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]); */
+
+/* var arr=[5,10,11,24,65, 38];
+ var max=arr[0];
+   for(var i=0; i<arr.length;i++ ){
+    if(arr[i]>=max){
+       max=arr[i]
+    }
+ }
+      console.log(max); */
+
+
+   /*    var min=arr[0];
+
+   for(var i=0; i<arr.length;i++ ){
+    if(arr[i]<min){
+       max=arr[i]
+    }
+ }
+      console.log(min); */
+
+     /*  var arr=[22,"apple",85, 64];
+      for(var i=arr.length-1; i>=0; i--){
+         console.log(arr[i])
+      }
+ */
+
+  
+      var arr=[1,2,8,12,0,11,4,0,10,3,7];
+      var sum=0;
+   for( var i=0; i<arr.length; i++);{
+       if(arr[i]){
+       sum+=arr[i] >8;
+       console.log(sum)
+       
+   }
+  
+}
